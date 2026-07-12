903cf64592d7be5c64e76cc613ee15fa6a1ac551
crazydj143 <meghanathidhaval6@gmail.com>
2026-07-12 14:57:46 +0530

    Added Telegram Gemini Bot

---
+++ bot.py
@@ -0,0 +1,38 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
+from google import genai
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🤖 Welcome!\n\nMain Gemini AI Telegram Bot hoon.\nMujhe koi bhi message bhejo."
+    )
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Commands:\n/start\n/help"
+    )
+
+async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    prompt = update.message.text
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+    await update.message.reply_text(response.text)
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
+
+print("Bot Started...")
+app.run_polling()
