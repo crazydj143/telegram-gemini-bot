16bcd602b60179d3f39e577c9c41318563bf2919
crazydj143 <65232558+crazydj143@users.noreply.github.com>
2026-07-12 16:12:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
+import os
 import asyncio
 
-try:
-    asyncio.get_event_loop()
-except RuntimeError:
-    asyncio.set_event_loop(asyncio.new_event_loop())
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 from google import genai
@@ -13,15 +10,18 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "🤖 Welcome!\n\nMain Gemini AI Telegram Bot hoon.\nMujhe koi bhi message bhejo."
     )
 
+
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "Commands:\n/start\n/help"
     )
+
 
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
     prompt = update.message.text
@@ -33,11 +33,24 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(response.text)
 
-app = Application.builder().token(BOT_TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("help", help_command))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
 
-print("Bot Started...")
-app.run_polling()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, chat)
+    )
+
+    print("Bot Started...")
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.get_event_loop()
+    except RuntimeError:
+        asyncio.set_event_loop(asyncio.new_event_loop())
+
+    main()
